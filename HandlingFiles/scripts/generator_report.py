691e1aec21d8dca98a9caf45ad62a3606dfcc93a
Giovanni Brunetti <gb1609@outlook.com>
2021-08-12 22:14:07 +0200

    First exercises

---
+++ HandlingFiles/scripts/generator_report.py
@@ -0,0 +1,48 @@
+# !/usr/bin/env python3
+import csv
+from io import open
+
+
+def read_employees(csv_file_location):
+    with open(csv_file_location) as employees:
+        employee_file = csv.register_dialect('empDialect', skipinitialspace=True, strict=True)
+
+        reader = csv.DictReader(employees)
+
+        employee_list = []
+
+        for data in reader:
+            employee_list.append(data)
+
+        return employee_list
+
+
+def process_data(employee_list):
+    department_list = []
+
+    for employee_data in employee_list:
+        print(str(employee_data))
+        department_list.append(employee_data[' Department'])
+
+    department_data = {}
+
+    for department_name in set(department_list):
+        department_data[department_name] = department_list.count(department_name)
+
+    return department_data
+
+
+employee_list = read_employees(
+    "/mnt/c/Users/gb1609/IdeaProjects/thesis/QwicklabsExcercises/HandlingFiles/data/employees.csv")
+
+
+def write_report(dictionary, report_file):
+    with open(report_file, "w+") as f:
+        for k in sorted(dictionary):
+            f.write(str(k) + ':' + str(dictionary[k]) + '\n')
+        f.close()
+
+
+dictionary = process_data(employee_list)
+
+write_report(dictionary, '/mnt/c/Users/gb1609/IdeaProjects/thesis/QwicklabsExcercises/HandlingFiles/data/report.txt')
