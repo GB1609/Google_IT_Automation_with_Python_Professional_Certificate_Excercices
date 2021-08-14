baafb68f337c6e19cf33bc719ba1f15919e3c60c
Giovanni Brunetti <gb1609@outlook.com>
2021-08-14 19:18:45 +0200

    Fix a slow system with Python

--- bash_scripts/from.sh
+++ bash_scripts/from.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Questo è un commento
 local_user=gb1609
-key=qwikLABS-L2382-32791633.ppk
+key=qwikLABS-L2399-32792729.ppk
 ppk_key_path=/mnt/c/Users/${local_user}/Desktop/${key}
 
 remote_user=student-01-18f81104e185
-remote_ip=162.222.182.20
-local_path=/mnt/c/Users/gb1609/IdeaProjects/Google_IT_Automation_with_Python_Professional_Certificate_Excercices/
-remote_path=.
+remote_ip=104.197.230.239
+local_path=/mnt/c/Users/gb1609/IdeaProjects/Google_IT_Automation_with_Python_Professional_Certificate_Excercices/fix_a_slow_system_with_python/scripts/
+remote_path=/home/${remote_user}/scripts/*
 
-pscp -sftp -r -i ${ppk_key_path} ${local_path} ${remote_user}@${remote_ip}:/home/${remote_user}/${remote_path}
+pscp -sftp -r -i ${ppk_key_path} ${remote_user}@${remote_ip}:${remote_path} ${local_path}
